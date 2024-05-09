2cec8bfca2dded1ea002de9daaade122ae25a6ae
mwest0101 <62764107+mwest0101@users.noreply.github.com>
2024-05-09 12:34:50 -0300

    Funcionando con control cuando se va afuera. Se arreglaron los nombres de archivos .

--- Pong.gd
+++ Pong.gd
@@ -1,13 +1,13 @@
 extends Node2D
 const CENTER=Vector2(576,324)
-func _on_goal_left_body_entered(body):
-	$Ball.position = CENTER
-	$Player.position.y = CENTER.y
-	$Computer.position.y = CENTER.y
-
-
 
 func _on_goal_right_body_entered(body):
 	$Ball.position = CENTER
 	$Player.position.y = CENTER.y
 	$Computer.position.y = CENTER.y
+
+func _on_goal_left_body_entered(body):
+	$Ball.position = CENTER
+	$Player.position.y = CENTER.y
+	$Computer.position.y = CENTER.y
+	
