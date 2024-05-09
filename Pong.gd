6eb8f2fbb41ceeebd07d8ed10b7646be0d440f47
mwest0101 <62764107+mwest0101@users.noreply.github.com>
2024-05-09 15:23:19 -0300

    FINAL (Se soluciono problema de doble punto al inicio) Estaba tocando las parede horizontales con las verticales y ejecutaba doble punto al inicio

--- Pong.gd
+++ Pong.gd
@@ -1,20 +1,26 @@
 extends Node2D
 const CENTER=Vector2(576,324)
 var computer_score=0
-var player_score=-2
+var player_score=0
+var cont=0
 
+
+	
 func _on_goal_left_body_entered(body):
+	
 	computer_score += 1
 	$ComputerScore.text = str(computer_score)	
+	cont+=1
 	reset()
 	
 func _on_goal_right_body_entered(body):
 	player_score += 1
 	$PlayerScore.text = str(player_score)
+	cont+=1
 	reset()
 	
-func reset():
-	
+func reset():	
+	print("Con=",cont)	
 	$Ball.position = CENTER
 	$Ball.call("set_ball_velocity")
 	$Player.position.y = CENTER.y
